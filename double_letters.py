ba8e20a6cdf63a0aa8e213aa704a0d12fb5d30d6
ngoam76 <nabchau76@gmail.com>
2023-09-02 19:53:36 +0200

    done

--- double_letters.py
+++ double_letters.py
@@ -1,9 +1,8 @@
 def double_letters(word):
     for i in range(len(word)-1):
       if word[i] == word[i+1]:
-         print("True")
          return True
     return False
 
 
-double_letters("Nono")
+print(double_letters("Nono"))
